67ac9f15819c98a4e07c292b3a38ecd9d0559781
alpha0314alpha <alpha0314alpha@gmail.com>
2026-09-25 22:41:28 +0900

    command

--- main.cpp
+++ main.cpp
@@ -5,9 +5,19 @@
 using namespace std;
 const size_t MAX_LEN = 20;
 
+#if 0
 int main(){
     string input;
     cin >> input;
+#else
+int main(int argc, char* argv[]){
+#endif
+    assert(argc >= 2);
+    string input;
+    for (int i = 1; i < argc; i++){
+        if (i > 1) input += " ";
+        input += argv[i];
+    }
     size_t width = min(MAX_LEN, input.size());
     vector<string> sub;
     for (size_t i = 0; i < input.size(); i += width){
